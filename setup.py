87703510239c0f2c08166cc6007b650297aff3df
Alex Dunne <alexdunne@gmail.com>
2019-10-23 13:11:02 -0400

    Ensure desired installation of script

--- setup.py
+++ setup.py
@@ -4,19 +4,27 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="wiktionary_abbreviation_scraper",
-    version="0.1.0",
+    name="abbrevscrape",
+    version="0.1.1",
     author="Alexander Dunne",
-    author_email="author@example.com",
+    author_email="alexdunne@gmail.com",
     description="Script that scrapes abbreviations from wiktionary.org",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    license="MIT",
+    url="https://github.com/dunnesquared/abbrevscrape",
     packages=setuptools.find_packages(),
+    # Include this line to ensure abbrevscrape is installed as a module
+    py_modules=["abbrevscrape"],
+    install_requires=["bs4", "requests"],
+    tests_require=["nose"],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL 3.0",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-)
+    # If any package contains *.txt files, include them
+    package_data={'': ['*.txt']},
+    include_package_data=True,
+    )
