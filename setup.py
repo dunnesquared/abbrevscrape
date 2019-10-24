b43735d481e3d9973aeb84e4ff0ff3ab26d048a6
Alex Dunne <alexdunne@gmail.com>
2019-10-24 12:44:58 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="abbrevscrape",
-    version="0.1.1",
+    version="0.1.2",
     author="Alexander Dunne",
     author_email="alexdunne@gmail.com",
     description="Script that scrapes abbreviations from wiktionary.org",
